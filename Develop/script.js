baa2140945c5ab61d42651844e58bdee939dddf3
kate-ak <kate.akin@protonmail.com>
2020-07-30 18:54:35 -0700

    adding variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,26 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+const upperChar ('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+const lowerChar ('abcdefghijklmnopqrstuvwxyz');
+const specialChar ('~!@#$%^&*()-+=?;:');
+const numChar ('1234567890');
+
+let totalUserChar = "";
+
+var passwordLength = prompt("Please slect your password length between 8 and 128 characters");
+
+if (passwordLength < 8 || passwordLength > 128) {
+  alert("please enter a valid password length");
+  return
+}
+
+
+
+
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
@@ -15,12 +31,7 @@ generateBtn.addEventListener("click", writePassword);
 
 
 var passwordLength = prompt("Please slect your password length between 8 and 128 characters");
-
-if (passwordLength < 8 || passwordLength > 128) {
-  alert("please enter a valid password length");
-} else {
-  var length = document.createElement(passwordLength);
-}
+  
 
 // Set i = 0, then for loop until index.length of LENGTH
 
