2cd71c13e51bbfc8e44451ab626eb792ada810c8
kate-ak <kate.akin@protonmail.com>
2020-07-30 19:41:13 -0700

    corrections

--- Develop/script.js
+++ Develop/script.js
@@ -1,37 +1,30 @@
-const upperChar ('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
-const lowerChar ('abcdefghijklmnopqrstuvwxyz');
-const specialChar ('~!@#$%^&*()-+=?;:');
-const numChar ('1234567890');
-
-let totalUserChar = "";
-
-var passwordLength = prompt("Please slect your password length between 8 and 128 characters");
-
-if (passwordLength < 8 || passwordLength > 128) {
-  alert("please enter a valid password length");
-  return
-}
-
-
-
-
-let generateBtn = document.querySelector("#generate");
+const upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerChar = "abcdefghijklmnopqrstuvwxyz";
+const specialChar = "~!@#$%^&*()-+=?;:";
+const numChar = "1234567890";
 
 // Write password to the #password input
 function writePassword() {
-  let password = generatePassword();
-  let passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  let passwordLength = prompt("Please select your password length between 8 and 128 characters");
+  let characterLength = parseInt(passwordLength);
+  if (characterLength < 8 || characterLength > 128) {
+    alert("please enter a valid password length");
+    return;
+  }
+  let userUpperChar = confirm("Do you want to use uppercase characters?");
+  let userLowerChar = confirm("Do you want to use lowercase characters?");
+  let userSpecialChar = confirm("Do you want to use special characters?");
+  let userNumChar = confirm("Do you want to use numbers?")
 
 }
+writePassword ();
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//   let password = generatePassword();
+//   let passwordText = document.querySelector("#password");
+//   passwordText.value = password;
+// }
 
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
 
-var passwordLength = prompt("Please slect your password length between 8 and 128 characters");
-  
-
-// Set i = 0, then for loop until index.length of LENGTH
-
+// // Set i = 0, then for loop until index.length of LENGTH
