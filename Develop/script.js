11ab5f566a5a1d47a49a6c824311c179f2c2221d
kate-ak <kate.akin@protonmail.com>
2020-07-31 11:39:45 -0700

    event listener function added

--- Develop/script.js
+++ Develop/script.js
@@ -2,15 +2,15 @@ const upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerChar = "abcdefghijklmnopqrstuvwxyz";
 const specialChar = "~!@#$%^&*()-+=?;:";
 const numChar = "1234567890";
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function generate() {
   let passwordChars = "";
   let passwordLength = prompt("Please select your password length between 8 and 128 characters");
-  const characterLength = parseInt(passwordLength);
+  let characterLength = parseInt(passwordLength);
   if (characterLength < 8 || characterLength > 128) {
     alert("please enter a valid password length");
-    return;
   }
   let userUpperChar = confirm("Do you want to use uppercase characters?");
   let userLowerChar = confirm("Do you want to use lowercase characters?");
@@ -31,29 +31,22 @@ function writePassword() {
   }
   if (passwordChars.length < 1) {
   alert("please confirm at least one option for password")
+  return
   }
-  return characterLength;
-}
-// let pwLength = characterLength;
-// function for randomizing to user character limit
-// YOU ARE HAVING ISSUES WITH TAKING A VARIABLE FROM A PREVIOUS FUNCTION AND USING IT IN  A NEW FUNCTION
-// You need to do input
-
-function generatePassword() {
-  for (var i = 0; i < characterLength.length; i++){
-    var password = passwordChar[Math.floor(Math.random) * characterLength.length];
+  else {
+    var length = characterLength;
+      charset = passwordChars;
+      retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+      retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+  return retVal;
+  var password = generate();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
   }
-  return password;
 }
-writePassword ();
-generatePassword();
 
-//   let password = generatePassword();
-//   let passwordText = document.querySelector("#password");
-//   passwordText.value = password;
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
-// // Set i = 0, then for loop until index.length of LENGTH--- done
+generateBtn.addEventListener("click", generate);
+console.log(password);
+// generatePassword();
